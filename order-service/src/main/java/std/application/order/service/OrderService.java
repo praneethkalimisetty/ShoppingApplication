be171af3ea4c0114081da367a358eec731b72010
KALIMISETTY SAI PRANEETH <kalimisettypraneeth@gmail.com>
2023-04-26 12:25:16 -0400

    Added Discovery Server to monitor using Eureka Server Client

--- order-service/src/main/java/std/application/order/service/OrderService.java
+++ order-service/src/main/java/std/application/order/service/OrderService.java
@@ -25,7 +25,7 @@ import std.application.order.repository.OrderRepository;
 public class OrderService {
 
 	private final OrderRepository orderRepository;
-	private final WebClient webClient;
+	private final WebClient.Builder webClientBuilder;
 
 	public void placeOrder(OrderRequest orderRequest) {
 		Order order = new Order();
@@ -40,8 +40,8 @@ public class OrderService {
 
 		log.info("List of skuCodes: {}", skuCodes);
 		// Calling Inventory Service to check if the product is in stock
-		InventoryResponse[] inventoryResponseArray = webClient.get()
-				.uri("http://localhost:8082/api/inventory",
+		InventoryResponse[] inventoryResponseArray = webClientBuilder.build().get()
+				.uri("http://inventory-service/api/inventory",
 						uriBuilder -> uriBuilder.queryParam("skuCode", skuCodes).build())
 				.retrieve()
 				.bodyToMono(InventoryResponse[].class)
